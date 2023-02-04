bc3f2bf41031ef381d922ba66894c33f33a25e21
Ali Raza <araza280394@gmail.com>
2023-02-05 00:18:19 +0500

    Excel file Export
    Added Excel File Export

--- src/main/java/com/houarizegai/webscraping/amazon/AmazonBookScraper.java
+++ src/main/java/com/houarizegai/webscraping/amazon/AmazonBookScraper.java
@@ -10,17 +10,23 @@ import java.util.stream.Collectors;
 
 public class AmazonBookScraper {
     public static void main(String[] args) throws IOException {
-        String url = "https://www.amazon.com/Best-Sellers-Kindle-Store-Computer-Programming/zgbs/digital-text/156140011";
-        Document page = Jsoup.connect(url).userAgent("JSoup scraper").get();
+        try {
 
-        String bookTitleSelector = "p13n-sc-truncate";
-        // Get the books name using the html class selector
-        Elements bookElements = page.getElementsByClass(bookTitleSelector);
 
-        List<String> booksTitle = bookElements.stream()
-                .map(element -> element.text())
-                .collect(Collectors.toList());
+            String url = "https://www.amazon.com/Best-Sellers-Kindle-Store-Computer-Programming/zgbs/digital-text/156140011";
+            Document page = Jsoup.connect(url).userAgent("JSoup scraper").get();
 
-        booksTitle.forEach(System.out::println);
+            String bookTitleSelector = "p13n-sc-truncate";
+            // Get the books name using the html class selector
+            Elements bookElements = page.getElementsByClass(bookTitleSelector);
+
+            List<String> booksTitle = bookElements.stream()
+                    .map(element -> element.text())
+                    .collect(Collectors.toList());
+
+            booksTitle.forEach(System.out::println);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/houarizegai/webscraping/imdb/IMDBScraper.java
+++ src/main/java/com/houarizegai/webscraping/imdb/IMDBScraper.java
@@ -1,14 +1,17 @@
 package com.houarizegai.webscraping.imdb;
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.LinkedList;
 import java.util.List;
 
-public class IMDBScraper {
+public class    IMDBScraper {
     // Get the Top 250
     public static void main(String[] args) throws IOException {
         String url = "https://www.imdb.com/search/title/?groups=top_250&sort=user_rating";
@@ -26,6 +29,85 @@ public class IMDBScraper {
             final String rate = element.getElementsByClass("ratings-imdb-rating").attr("data-value");
 
             topFilms.add(new Film(title, year, category, description, rate));
+
+            // Writing to excel file
+
+            Workbook workbook = new XSSFWorkbook();
+
+            Sheet sheet = workbook.createSheet("Movies");
+            sheet.setColumnWidth(0, 6000);
+            sheet.setColumnWidth(1, 4000);
+
+            Row header = sheet.createRow(0);
+
+            CellStyle headerStyle = workbook.createCellStyle();
+            headerStyle.setFillForegroundColor(IndexedColors.LIGHT_BLUE.getIndex());
+            headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+            XSSFFont font = ((XSSFWorkbook) workbook).createFont();
+            font.setFontName("Title");
+            font.setFontHeightInPoints((short) 16);
+            font.setBold(true);
+            headerStyle.setFont(font);
+
+            Cell headerCell = header.createCell(0);
+            headerCell.setCellValue("Title");
+            headerCell.setCellStyle(headerStyle);
+
+            headerCell = header.createCell(1);
+            headerCell.setCellValue("Year");
+            headerCell.setCellStyle(headerStyle);
+
+            headerCell = header.createCell(2);
+            headerCell.setCellValue("Category");
+            headerCell.setCellStyle(headerStyle);
+
+            headerCell = header.createCell(3);
+            headerCell.setCellValue("Description");
+            headerCell.setCellStyle(headerStyle);
+
+            headerCell = header.createCell(4);
+            headerCell.setCellValue("Rate");
+            headerCell.setCellStyle(headerStyle);
+
+            // cewll style
+            CellStyle style = workbook.createCellStyle();
+            style.setWrapText(true);
+
+            Row row = sheet.createRow(topFilms.size());
+            Cell cell = row.createCell(0);
+            cell.setCellValue("John Smith");
+            cell.setCellStyle(style);
+
+            cell = row.createCell(1);
+            cell.setCellValue(20);
+            cell.setCellStyle(style);
+
+            cell = row.createCell(2);
+            cell.setCellValue(20);
+            cell.setCellStyle(style);
+
+            cell = row.createCell(3);
+            cell.setCellValue(20);
+            cell.setCellStyle(style);
+
+            cell = row.createCell(4);
+            cell.setCellValue(20);
+            cell.setCellStyle(style);
+
+            File currDir = new File(".");
+            String path = currDir.getAbsolutePath();
+            String fileLocation = path.substring(0, path.length() - 1) + "movies.xlsx";
+
+            try {
+                FileOutputStream outputStream = new FileOutputStream(fileLocation);
+                workbook.write(outputStream);
+                workbook.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+
         });
 
         System.out.println(topFilms);
